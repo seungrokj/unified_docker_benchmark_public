46358941231285267aeaafe535b87d3198609115
seungrokjung <seungrok.jung@amd.com>
2024-10-23 12:23:09 +0000

    v2

--- run.sh
+++ run.sh
@@ -1,40 +1,86 @@
-#!/usr/bin/bash
-## float16
-./vllm_benchmark_report.sh -s latency -m meta-llama/Meta-Llama-3.1-8B-Instruct   -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m meta-llama/Meta-Llama-3.1-70B-Instruct  -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m meta-llama/Meta-Llama-3.1-405B-Instruct -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m meta-llama/Llama-2-7b-chat-hf           -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m meta-llama/Llama-2-70b-chat-hf          -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m mistralai/Mixtral-8x7B-Instruct-v0.1    -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m mistralai/Mixtral-8x22B-Instruct-v0.1   -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m mistralai/Mistral-7B-Instruct-v0.3      -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m Qwen/Qwen2-7B-Instruct                  -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m Qwen/Qwen2-72B-Instruct                 -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m core42/jais-13b-chat                    -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m core42/jais-30b-chat-v3                 -g 1 -d float16
+#!/bin/bash
+###############################################################################
+#
+# MIT License
+#
+# Copyright (c) 2024 Advanced Micro Devices, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#################################################################################
+set -ex
 
-./vllm_benchmark_report.sh -s throughput -m meta-llama/Meta-Llama-3.1-8B-Instruct   -g 1 -d float16
-./vllm_benchmark_report.sh -s throughput -m meta-llama/Meta-Llama-3.1-70B-Instruct  -g 8 -d float16
-./vllm_benchmark_report.sh -s throughput -m meta-llama/Meta-Llama-3.1-405B-Instruct -g 8 -d float16
-./vllm_benchmark_report.sh -s throughput -m meta-llama/Llama-2-7b-chat-hf           -g 1 -d float16
-./vllm_benchmark_report.sh -s throughput -m meta-llama/Llama-2-70b-chat-hf          -g 8 -d float16
-./vllm_benchmark_report.sh -s throughput -m mistralai/Mixtral-8x7B-Instruct-v0.1    -g 8 -d float16
-./vllm_benchmark_report.sh -s throughput -m mistralai/Mixtral-8x22B-Instruct-v0.1   -g 8 -d float16
-./vllm_benchmark_report.sh -s throughput -m mistralai/Mistral-7B-Instruct-v0.3      -g 1 -d float16
-./vllm_benchmark_report.sh -s throughput -m Qwen/Qwen2-7B-Instruct                  -g 1 -d float16
-./vllm_benchmark_report.sh -s throughput -m Qwen/Qwen2-72B-Instruct                 -g 8 -d float16
-./vllm_benchmark_report.sh -s throughput -m core42/jais-13b-chat                    -g 1 -d float16
-./vllm_benchmark_report.sh -s throughput -m core42/jais-30b-chat-v3                 -g 1 -d float16
+if [[ "$MAD_SYSTEM_GPU_ARCHITECTURE" != *"gfx94"* ]]; then 
+	echo "Unsuported GPU arch detected, please use supported MI300X GPUs \n"
+	exit 1
+fi
 
-## float8
-./vllm_benchmark_report.sh -s latency -m amd/Meta-Llama-3.1-8B-Instruct-FP8-KV   -g 1 -d float8
-./vllm_benchmark_report.sh -s latency -m amd/Meta-Llama-3.1-70B-Instruct-FP8-KV  -g 8 -d float8
-./vllm_benchmark_report.sh -s latency -m amd/Meta-Llama-3.1-405B-Instruct-FP8-KV -g 8 -d float8
-./vllm_benchmark_report.sh -s latency -m amd/Mixtral-8x7B-Instruct-v0.1-FP8-KV   -g 8 -d float8
-./vllm_benchmark_report.sh -s latency -m amd/Mixtral-8x22B-Instruct-v0.1-FP8-KV  -g 8 -d float8
+export HF_TOKEN=$MAD_SECRETS_HFTOKEN
 
-./vllm_benchmark_report.sh -s throughput -m amd/Meta-Llama-3.1-8B-Instruct-FP8-KV   -g 1 -d float8
-./vllm_benchmark_report.sh -s throughput -m amd/Meta-Llama-3.1-70B-Instruct-FP8-KV  -g 8 -d float8
-./vllm_benchmark_report.sh -s throughput -m amd/Meta-Llama-3.1-405B-Instruct-FP8-KV -g 8 -d float8
-./vllm_benchmark_report.sh -s throughput -m amd/Mixtral-8x7B-Instruct-v0.1-FP8-KV   -g 8 -d float8
-./vllm_benchmark_report.sh -s throughput -m amd/Mixtral-8x22B-Instruct-v0.1-FP8-KV  -g 8 -d float8
+# Parse named arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --model_repo) MODEL_NAME="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
+    esac
+    shift
+    case $1 in
+        --test_option) TEST_OPTION="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
+    esac
+    shift
+    case $1 in
+        --num_gpu) N_GPUS="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
+    esac
+    shift
+    case $1 in
+        --datatype) DTYPE="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
+    esac
+    shift
+done
+
+TEST_OPTION_SP=""
+for i in $(echo $TEST_OPTION | tr "," "\n")
+do
+  TEST_OPTION_SP="$TEST_OPTION_SP $i"
+done
+
+DTYPE_SP=""
+for i in $(echo $DTYPE | tr "," "\n")
+do
+  DTYPE_SP="$DTYPE_SP $i"
+done
+
+export HF_HUB_CACHE="/myworkspace"
+
+echo "=hyper params start="
+echo $MODEL_NAME
+echo $TEST_OPTION_SP
+echo $DTYPE_SP
+echo "=hyper params end="
+
+for scenario in $TEST_OPTION_SP; do
+    for dtype in $DTYPE_SP; do
+        ./vllm_benchmark_report.sh -s $scenario -m $MODEL_NAME -g $N_GPUS -d $dtype
+    done
+done
+
+echo "performance: 1 pass"
--- vllm_benchmark_report.sh
+++ vllm_benchmark_report.sh
@@ -27,7 +27,6 @@
 
 ## Usage: 
 #./vllm_benchmark_report.sh -s $mode -m $hf_model -g $n_gpu -d $datatype
-
 ## example:
 ## latency + throughput
 #./vllm_benchmark_report.sh -s all -m NousResearch/Meta-Llama-3-8B -g 1 -d float16
@@ -52,30 +51,43 @@ model_org_name=(${model//// })
 model_name=${model_org_name[1]}
 tp=$numgpu
 
+# perf env setting
+export HIP_FORCE_DEV_KERNARG=1
+export VLLM_USE_ROCM_CUSTOM_PAGED_ATTN=1
+export VLLM_USE_TRITON_FLASH_ATTN=0
+export VLLM_INSTALL_PUNICA_KERNELS=1
+export TOKENIZERS_PARALLELISM=false
+export RAY_EXPERIMENTAL_NOSET_ROCR_VISIBLE_DEVICES=1
+export NCCL_MIN_NCHANNELS=112
+export VLLM_FP8_PADDING=1
+export VLLM_FP8_ACT_PADDING=1
+export VLLM_FP8_WEIGHT_PADDING=1
+export VLLM_FP8_REDUCE_CONV=1
+export VLLM_SCHED_PREFILL_KVC_FREEPCT=31.0
+
 if [ $tp -eq 1 ]; then
     DIST_BE=" --enforce-eager "
 else
     DIST_BE=" --distributed-executor-backend mp "
 fi
 
-if [[ $datatype = "float16" ]]; then
+if [[ $datatype == "float16" ]]; then
     DTYPE=" --dtype float16 "	
 elif [[ $datatype == "float8" ]]; then
     DTYPE=" --dtype float16 --quantization fp8 --kv-cache-dtype fp8 " 
 fi
 
-OPTION_LATENCY=" --gpu-memory-utilization 0.99 --num-scheduler-steps 10 "
-OPTION_THROUGHPUT=" --gpu-memory-utilization 0.99 --num-scheduler-steps 128 --max-num-seqs 1000 "
+OPTION_LATENCY_P=" --gpu-memory-utilization 0.9 --enforce-eager "
+OPTION_LATENCY_D=" --gpu-memory-utilization 0.9 "
+OPTION_THROUGHPUT=" --gpu-memory-utilization 0.9 --num-scheduler-steps 1 "
 
 # latency conditions
 Bat="1 2 4 8 16 32 64 128 256"
 InLatency="128 2048"
-OutLatency="1 128 2048"
+OutLatency="128"
 
 # throughput conditions
-Req="256 2000"
-InThroughput="128 2048"
-OutThroughput="128 2048"
+Req_In_Out=("30000:128:128" "3000:2048:128" "3000:128:2048" "1500:2048:2048")
 
 report_dir="reports_${datatype}"
 report_summary_dir="${report_dir}/summary"
@@ -91,18 +103,28 @@ mkdir -p $report_summary_dir
 if [ "$scenario" == "latency" ] || [ "$scenario" == "all" ]; then
     echo "[INFO] LATENCY"
     mode="latency"
+    out=1
+    for inp in $InLatency;
+    do
+        for bat in $Bat;
+        do
+            outjson=${report_dir}/${model_name}_${mode}_decoding_bs${bat}_in${inp}_out${out}_${datatype}.json
+            outcsv=${report_summary_dir}/${model_name}_${mode}_report.csv
+            echo $model $mode $bat $tp $inp $out
+            python3 $tool_latency --model $model --batch-size $bat -tp $tp --input-len $inp --output-len $out --num-iters-warmup $n_warm --num-iters $n_itr --trust-remote-code --output-json $outjson $DTYPE $DIST_BE $OPTION_LATENCY_P
+            python3 $tool_report --mode $mode --model $model_name --batch-size $bat --tp $tp --input-len $inp --output-len $out $dtype --input-json $outjson --output-csv $outcsv --dtype $datatype
+        done
+    done
     for out in $OutLatency;
     do
-        for inp in $InLatency;
+    inp=1
+        for bat in $Bat;
         do
-            for bat in $Bat;
-            do
-                outjson=${report_dir}/${model_name}_${mode}_decoding_bs${bat}_in${inp}_out${out}_${datatype}.json
-                outcsv=${report_summary_dir}/${model_name}_${mode}_report.csv
-                echo $model $mode $bat $tp $inp $out
-                python3 $tool_latency --model $model --batch-size $bat -tp $tp --input-len $inp --output-len $out --num-iters-warmup $n_warm --num-iters $n_itr --trust-remote-code --enforce-eager --output-json $outjson $DTYPE $DIST_BE $OPTION_LATENCY
-                python3 $tool_report --mode $mode --model $model_name --batch-size $bat --tp $tp --input-len $inp --output-len $out $dtype --input-json $outjson --output-csv $outcsv
-            done
+            outjson=${report_dir}/${model_name}_${mode}_decoding_bs${bat}_in${inp}_out${out}_${datatype}.json
+            outcsv=${report_summary_dir}/${model_name}_${mode}_report.csv
+            echo $model $mode $bat $tp $inp $out
+            python3 $tool_latency --model $model --batch-size $bat -tp $tp --input-len $inp --output-len $out --num-iters-warmup $n_warm --num-iters $n_itr --trust-remote-code --output-json $outjson $DTYPE $DIST_BE $OPTION_LATENCY_D
+            python3 $tool_report --mode $mode --model $model_name --batch-size $bat --tp $tp --input-len $inp --output-len $out $dtype --input-json $outjson --output-csv $outcsv --dtype $datatype
         done
     done
 fi
@@ -110,18 +132,15 @@ fi
 if [ "$scenario" == "throughput" ] || [ "$scenario" == "all" ]; then
     echo "[INFO] THROUGHPUT"
     mode="throughput"
-    for req in $Req;
+    for req_in_out in ${Req_In_Out[@]}
     do
-        for out in $OutThroughput;
-        do
-            for inp in $InThroughput;
-            do
-                outjson=${report_dir}/${model_name}_${mode}_req${req}_in${inp}_out${out}_${datatype}.json
-                outcsv=${report_summary_dir}/${model_name}_${mode}_report.csv
-                echo $model $mode $req $tp $inp $out
-                python3 $tool_throughput --model $model --num-prompts $req -tp $tp --input-len $inp --output-len $out --trust-remote-code --output-json $outjson $DTYPE $DIST_BE $OPTION_THROUGHPUT
-                python3 $tool_report --mode $mode --model $model_name --num-prompts $req --tp $tp --input-len $inp --output-len $out $dtype --input-json $outjson --output-csv $outcsv
-            done
-        done
+        req=$(echo $req_in_out | awk -F':' '{ print $1 }')
+        inp=$(echo $req_in_out | awk -F':' '{ print $2 }')
+        out=$(echo $req_in_out | awk -F':' '{ print $3 }')
+        outjson=${report_dir}/${model_name}_${mode}_req${req}_in${inp}_out${out}_${datatype}.json
+        outcsv=${report_summary_dir}/${model_name}_${mode}_report.csv
+        echo $model $mode $req $tp $inp $out
+        python3 $tool_throughput --model $model --num-prompts $req -tp $tp --input-len $inp --output-len $out --trust-remote-code --output-json $outjson $DTYPE $DIST_BE $OPTION_THROUGHPUT
+        python3 $tool_report --mode $mode --model $model_name --num-prompts $req --tp $tp --input-len $inp --output-len $out $dtype --input-json $outjson --output-csv $outcsv --dtype $datatype
     done
 fi
